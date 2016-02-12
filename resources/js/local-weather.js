8ac7c8ce7288c6793dc04e2ab0a3f5113b087914
James <jammatpic@gmail.com>
2016-02-12 17:36:12 +1100

    Added weather retrieval based on location, and display of that location.

--- resources/js/local-weather.js
+++ resources/js/local-weather.js
@@ -1,8 +1,27 @@
+function firstUpperCase(str) {
+    return str[0].toUpperCase() + str.substring(1, str.length);
+}
+
 $(document).ready(function() {
-    $(".btn").on("click", function() {
-        window.alert("test");
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=5c5b877d759ecaf92e4d2106bb72e23e", function(json) {
-            $("#output").html(JSON.stringify(json));
+    var unit = 0;
+    
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var data;
+            var units = [["metric","C"],["imperial","F"]];
+            
+            var locationInfo = "lat=" + position.coords.latitude + "&lon=" + position.coords.longitude;
+            var apiKey = "&APPID=5c5b877d759ecaf92e4d2106bb72e23e";
+            var unitPref = "&units=" + units[unit][0];
+            
+            apiCall = "http://api.openweathermap.org/data/2.5/weather?" + locationInfo + apiKey + unitPref;
+            //$("#test").text(apiCall);
+            
+            $.getJSON(apiCall, function(json) {
+                data = json;
+                $("#condition").html(JSON.stringify(data.main.temp) + "°" + units[unit][1] + ", " + firstUpperCase(data.weather[0].description));
+                $("#location").html(data.name + ", " + data.sys.country);
+            });
         });
-    });
+    };
 });
